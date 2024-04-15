b29b5e2b05a518e9ad39341a08ae3f9574443c51
ThanhDuy21110759 <21110759@student.hcmute.edu.vn>
2024-04-15 16:40:04 +0700

    completed 75% Download Manager

--- app/src/main/java/com/example/download_manager/GalleryFragment.java
+++ app/src/main/java/com/example/download_manager/GalleryFragment.java
@@ -1,20 +1,74 @@
 package com.example.download_manager;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 public class GalleryFragment extends Fragment {
+    private GridView gridView;
+    private ProgressBar progressBar;
+    private ArrayList<String> dataList;
+    private PictureAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        View view =  inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        gridView = view.findViewById(R.id.gridView);
+        progressBar = view.findViewById(R.id.progressBar);
+        dataList = new ArrayList<>();
+        adapter = new PictureAdapter(getActivity(), dataList);
+
+        gridView.setAdapter(adapter);
+        getImagesFromStorage();
+        return view;
+    }
+
+    private void getImagesFromStorage() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Images/");
+        storageReference.listAll()
+                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                    @Override
+                    public void onSuccess(ListResult listResult) {
+                        for (StorageReference item : listResult.getItems()) {
+                            item.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    dataList.add(uri.toString());
+                                    adapter.notifyDataSetChanged();
+                                    progressBar.setVisibility(View.GONE);
+                                }
+                            });
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle any errors
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/example/download_manager/PictureAdapter.java
@@ -0,0 +1,52 @@
+package com.example.download_manager;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class PictureAdapter extends BaseAdapter {
+    private ArrayList<String> dataList;
+    private Context context;
+    LayoutInflater layoutInflater;
+
+    public PictureAdapter(Context context, ArrayList<String> dataList) {
+        this.context = context;
+        this.dataList = dataList;
+    }
+
+    @Override
+    public int getCount() {
+        return dataList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (layoutInflater == null){
+            layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        if (view == null){
+            view = layoutInflater.inflate(R.layout.grid_item, null);
+        }
+        ImageView gridImage = view.findViewById(R.id.gridImage);
+
+        Glide.with(context).load(dataList.get(i)).into(gridImage);
+        return view;
+    }
+}
